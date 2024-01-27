f3faf92890d475e28ab8664661c22e30f65ad618
ravig <ravig.1808@gmail.com>
2024-01-28 00:02:37 +0530

    Java Test

--- com.java.practice/HelloWorld.java
+++ com.java.practice/HelloWorld.java
@@ -1,5 +1,7 @@
+package com.java.practice;
+
 public class HelloWorld {
-    string msg="Welcome to Java World!!"
+    static String msg="Welcome to Java World!!";
     public static void main(String str[]){
         System.out.println(msg);
     }
